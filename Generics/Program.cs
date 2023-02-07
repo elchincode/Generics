70b29b9487007955df5ae9727a04333195e79366
elchincode <elchinai@code.edu.az>
2023-02-07 15:43:57 +0400

    Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GetDetails<string>("apple");
+
+
+            Swap<int>(5, 3);
+
+
+        }
+
+
+
+        static void GetDetails<T>(T type)
+        {
+            Console.WriteLine(type);
+        }
+
+        static void Swap<T>(T a, T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+
+
+
+        /*
+          static T GetDetails<T>(T type)  /////// Mellim bu cure yazilis duz sayilir
+        {
+            return type;
+        }
+
+
+        */
+    }
+}
